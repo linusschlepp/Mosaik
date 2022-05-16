feb186df7e5a5a74f09c29d307154b7b94b10cab
linus_ <l.schlepp@reinhausen.com>
2022-05-16 19:15:01 +0200

    finalmosaik gets created

--- styles.py
+++ styles.py
@@ -2,36 +2,17 @@ import random
 from PIL import Image, ImageDraw
 import cv2
 
-def style_4(step, color_inner_mosaik, draw, count, w ,h):
 
-    for y in range(0, w, step):
-        count = count + 1
-        pattern_1 = True
-        for x in range(0, h, step):
-            if count % 2:
-                if pattern_1:
-                    draw.polygon([(x, y), (x, y + step), (x+step, y)], fill=color_inner_mosaik)
-                    pattern_1 = False
-                else:
-                    draw.polygon([(x , y ), (x+step, y + step), (x + step, y)], fill=color_inner_mosaik)
-                    pattern_1 = True
-            else:
-                if pattern_1:
-                    draw.polygon([(x , y+step ), (x+step, y + step), (x, y)], fill=color_inner_mosaik)
-                    pattern_1 = False
-                else:
-                    draw.polygon([(x+step , y+step ), (x, y + step), (x + step, y)], fill=color_inner_mosaik)
-                    pattern_1 = True
 
 
 def ultimate_style(step):
     colors = ["red", "yellow", "cyan", "blue", "pink", "orange",
               "black", "white",  "green", "purple", "grey", "violet", "ivory", "coral", "brown", "teal", "tan",
-              "salmon", "lime",  "gold", "aquamarine" ,  "maroon", "orchid", "darkgrey", "darkgreen"]
+              "salmon", "lime",  "gold", "aquamarine" ,  "maroon", "orchid", "darkgrey", "darkgreen" ,"darkred" , "darkblue" ]
 
 
 
-    for x in range(64):
+    for x in range(36):
         color_1 = colors[random.randint(0, len(colors) - 1)]
         color_2 = get_color_rec(colors, color_1)
         img = Image.new("RGB", (1250, 1250), color_1)
@@ -40,7 +21,7 @@ def ultimate_style(step):
         w, h = img.size
 
         count = 0
-        ran_num = random.randint(1,4)
+        ran_num = random.randint(1,7)
         if (ran_num == 1):
             style_1(step, color_2, draw, count, w, h)
         if (ran_num == 2):
@@ -49,23 +30,33 @@ def ultimate_style(step):
             style_3(step, color_2, draw, count, w, h)
         if (ran_num == 4):
             style_4(step, color_2, draw, count, w, h)
+        if (ran_num == 5):
+            style_5(step, color_2, draw, count, w, h)
+        if (ran_num == 6):
+            style_6(step, color_2, draw, count, w, h)
+        if (ran_num == 7):
+            style_7(step, color_2, draw, count, w, h)
+
 
         img.save("images/"+"pic"+ str(x) + ".png")
         print("pic"+ str(x) + ".png" + " was created")
 
+
+    mosaiks = []
+    count_1 = 0
+    for y in range(6):
         read_img = []
-
-        for x in range(8):
-            read_img.append(cv2.imread("images/"+"pic"+str(x)+".png"))
-
+        for x in range(6):
+            read_img.append(cv2.imread("images/"+"pic"+str(count_1)+".png"))
+            count_1 = count_1 + 1
         im_v = cv2.vconcat(read_img)
-
-        # show the output image
-        cv2.save('sea_image.jpg', im_v)
-
+        cv2.imwrite("mosaiks/mosaik" + str(y)+".png", im_v)
+        mosaiks.append(cv2.imread("mosaiks/mosaik" + str(y)+".png"))
 
 
 
+    im_h = cv2.hconcat(mosaiks)
+    cv2.imwrite('finalmosaik.png', im_h)
 
 
 
@@ -80,17 +71,13 @@ def get_color_rec(colors, color_1):
     return get_color_rec(colors, color_1)
 
 
-
-
-
-
 def style_1(step, color_inner_mosaik, draw, count, w ,h):
 
     for y in range(0, w, step):
         count = count + 1
         pattern_1 = True
         for x in range(0, h, step):
-            if count % 2:
+            if count % 2 == 0:
                 if pattern_1:
                     draw.polygon([(x, y+step), (x+step, y + step), (x, y)], fill=color_inner_mosaik)
                     pattern_1 = False
@@ -124,8 +111,91 @@ def style_3(step, color_inner_mosaik, draw, count, w ,h):
                 draw.polygon([(x , y ), (x + step, y), (x, y+step)], fill=color_inner_mosaik)
 
 
-def style_5(step, color_inner_mosaik, draw, count, w ,h):
+def style_4(step, color_inner_mosaik, draw, count, w ,h):
     for y in range(0, w, step):
         count = count + 1
         for x in range(0, h, step):
-                draw.polygon([(x , y ), (x + step, y), (x, y+step)], fill=color_inner_mosaik)
+            if count % 2 == 0:
+                draw.polygon([(x+step , y+step ), (x , y+step), (x+step, y)], fill=color_inner_mosaik)
+            else:
+                draw.polygon([(x, y), (x + step, y + step), (x + step, y)], fill=color_inner_mosaik)
+
+def style_5(step, color_inner_mosaik, draw, count, w ,h):
+
+    for y in range(0, w, step):
+        count = count + 1
+        pattern_1 = True
+        for x in range(0, h, step):
+            if count % 2 == 0:
+                if pattern_1:
+                    pattern_1 = False
+                else:
+                    draw.polygon([(x, y + step), (x + step, y + step), (x, y)], fill=color_inner_mosaik)
+                    pattern_1 = True
+            else:
+                draw.polygon([(x , y ), (x+step, y + step), (x + step, y)], fill=color_inner_mosaik)
+
+def style_6(step, color_inner_mosaik, draw, count, w ,h):
+
+    for y in range(0, w, step):
+        count = count + 1
+        pattern_1 = True
+        for x in range(0, h, step):
+            if count % 2 == 0:
+                if pattern_1:
+                    draw.polygon([(x, y + step), (x + step, y + step), (x, y)], fill=color_inner_mosaik)
+                    pattern_1 = False
+                else:
+                    draw.polygon([(x + step, y + step), (x, y + step), (x + step, y)], fill=color_inner_mosaik)
+                    pattern_1 = True
+            else:
+                if pattern_1:
+                    draw.polygon([(x, y), (x+step, y + step), (x + step, y)], fill=color_inner_mosaik)
+                    pattern_1 = False
+                else:
+                    draw.polygon([(x, y), (x, y + step), (x + step, y)], fill=color_inner_mosaik)
+                    pattern_1 = True
+
+
+def style_7(step, color_inner_mosaik, draw, count, w ,h):
+
+    for y in range(0, w, step):
+        count = count + 1
+        pattern_1 = True
+        for x in range(0, h, step):
+            if count % 2 == 0:
+                if pattern_1:
+                    draw.polygon([(x, y), (x+step, y + step), (x + step, y)], fill=color_inner_mosaik)
+                    pattern_1 = False
+                else:
+                    draw.polygon([(x, y), (x, y + step), (x + step, y)], fill=color_inner_mosaik)
+                    pattern_1 = True
+            else:
+                if pattern_1:
+                    draw.polygon([(x, y), (x+step, y + step), (x + step, y)], fill=color_inner_mosaik)
+                    pattern_1 = False
+                else:
+                    draw.polygon([(x, y), (x, y + step), (x + step, y)], fill=color_inner_mosaik)
+                    pattern_1 = True
+
+
+def style_8(step, color_inner_mosaik, draw, count, w ,h):
+
+    for y in range(0, w, step):
+        count = count + 1
+        pattern_1 = True
+        for x in range(0, h, step):
+            if count % 2 == 0:
+                if pattern_1:
+                    draw.polygon([(x, y), (x+step, y + step), (x + step, y)], fill=color_inner_mosaik)
+                    pattern_1 = False
+                else:
+                    draw.polygon([(x, y), (x, y + step), (x + step, y)], fill=color_inner_mosaik)
+                    pattern_1 = True
+            else:
+                if pattern_1:
+                    draw.polygon([(x, y), (x+step, y + step), (x + step, y)], fill=color_inner_mosaik)
+                    pattern_1 = False
+                else:
+                    draw.polygon([(x, y), (x, y + step), (x + step, y)], fill=color_inner_mosaik)
+                    pattern_1 = True
